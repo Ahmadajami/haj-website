d2354528faa658664afacce38fddfaf7074df808
Ahmad Alajami <ahmad_2000_aj@hotmail.com>
2025-06-25 15:45:24 +0300

    Initial HAJ Website project setup

---
+++ src/lib/index.ts
@@ -0,0 +1,11 @@
+// place files you want to import through the `$lib` alias in this folder.
+export const seo = {
+    title: "Haj Agency | Your Ideas, Realized.",
+    description:
+        "Haj Agency offers comprehensive solutions for Web Development, HR Development, UI/UX Design, and Interior Design. We bring your vision to life.",
+    image: "https://i.pinimg.com/736x/85/9a/92/859a92a2629f912010a0a72270aefedc.jpg",
+    twitter: "HajAgency",
+    url: "https://haj.agency",
+    keywords:
+        "Haj Agency, web development, HR development, UI/UX design, interior design, business solutions, creative agency, digital solutions, human resources, user experience, branding, web design, office design",
+};
